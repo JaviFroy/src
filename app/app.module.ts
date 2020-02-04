e0739ea64faa011fe4bafcbdf676e781bf38ebec
Javi <javi.sanchezsanchez97@gmail.com>
2020-02-04 14:18:43 +0100

    crear pie componente

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { CentralComponent } from './componentes/central/central.component';
+import { PieComponent } from './componentes/pie/pie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    CentralComponent
+    CentralComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
